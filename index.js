ec4e1d9edabda7d4021a1e3e374256095876a23f
Guilherme Gama <guilhermegamabs@gmail.com>
2024-03-30 16:27:44 -0300

    ShowAll Categories

--- index.js
+++ index.js
@@ -2,10 +2,17 @@ const express = require('express');
 const app = express();
 const PORT = 3333;
 
+const db = require('./services/database');
+
 app.use(express.json());
 
-app.get('/', (req, res) => {
-    res.send({ message: 'Hello World!' });
+app.get('/category', async (req, res) => {
+    try {
+        const result = await db.pool.query('SELECT * FROM category');
+        return res.status(200).json(result.rows);
+    } catch(err) {
+        return res.status(500).json({ error: err.message});
+    }
 });
 
 app.listen(PORT, () => {
