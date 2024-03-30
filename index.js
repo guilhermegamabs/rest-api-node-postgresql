ab48f618021548c7157c9eeaae9306908234cc5a
Guilherme Gama <guilhermegamabs@gmail.com>
2024-03-30 16:17:33 -0300

    Test

--- index.js
+++ index.js
@@ -4,6 +4,9 @@ const PORT = 3333;
 
 app.use(express.json());
 
+app.get('/', (req, res) => {
+    res.send({ message: 'Hello World!' });
+});
 
 app.listen(PORT, () => {
     console.log(`Running on PORT ${PORT}`);
