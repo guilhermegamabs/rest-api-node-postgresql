20146b0d87da7c4b684d6b4cd32858342dc95411
Guilherme Gama <guilhermegamabs@gmail.com>
2024-03-30 16:35:49 -0300

    Category File

--- src/index.js
+++ src/index.js
@@ -2,18 +2,12 @@ const express = require('express');
 const app = express();
 const PORT = 3333;
 
-const db = require('./services/database');
-
 app.use(express.json());
+app.use(express.urlencoded({extended: false}));
 
-app.get('/category', async (req, res) => {
-    try {
-        const result = await db.pool.query('SELECT * FROM category');
-        return res.status(200).json(result.rows);
-    } catch(err) {
-        return res.status(500).json({ error: err.message});
-    }
-});
+const categoryRoute = require('./routes/category');
+
+app.use('/category', categoryRoute);
 
 app.listen(PORT, () => {
     console.log(`Running on PORT ${PORT}`);
