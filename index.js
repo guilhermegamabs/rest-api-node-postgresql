dad26c07399713190dac022aa69592128fe5daad
Guilherme Gama <guilhermegamabs@gmail.com>
2024-03-30 16:13:12 -0300

    Initial

---
+++ index.js
@@ -0,0 +1,10 @@
+const express = require('express');
+const app = express();
+const PORT = 3333;
+
+app.use(express.json());
+
+
+app.listen(PORT, () => {
+    console.log(`Running on PORT ${PORT}`);
+});
