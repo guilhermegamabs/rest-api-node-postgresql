7a1e49c96a7cc0db39dab4217aaff39931775fdf
Guilherme Gama <guilhermegamabs@gmail.com>
2024-04-01 20:07:47 -0300

    CreateProduct Route

--- src/controllers/productController.js
+++ src/controllers/productController.js
@@ -13,10 +13,14 @@ exports.createProducts = async(req, res) => {
     try {
         const { name, description, price, quantity, category_id } = req.body;
 
-        // const newResult = await db.pool.query({
-        //     text: 'SELECT EXISTS (SELECT * FROM product WHERE name = $1, description = $2, price = $3, quantity = $4',
-        //     values: [name, description, price, quantity]
-        // });
+        if(!name) return res.status(422).json({ error: 'Name is required!' });
+
+        const existsResult = await db.pool.query({
+            text: 'SELECT EXISTS (SELECT * FROM product WHERE name = $1)',
+            values: [name]
+        });
+
+        if(existsResult.rows[0].exists) return res.status(400).json({ error: `Product ${name} already exists!` });
 
         const newResult = await db.pool.query({
             text: 'INSERT INTO product(name, description, price, quantity, category_id) VALUES ($1, $2, $3, $4, $5) RETURNING *',
@@ -26,6 +30,6 @@ exports.createProducts = async(req, res) => {
         return res.status(201).json(newResult.rows);
 
     } catch(err) {
-        return res.status(500).json({ error: err.message });
+        return res.status(500).json({ error: 'hELLO WORLD!' });
     }
 }
